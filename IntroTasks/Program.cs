e469e019399c92ecbf8fef0881306db658b3150b
Rafet <rafetr@risk.az>
2023-02-04 11:39:33 +0400

    init

---
+++ IntroTasks/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace IntroTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //int a = int.Parse(Console.ReadLine());
+            ////int countOfDigit = (int)Math.Log10(a) + 1;
+
+            //if (a >= 10000 && a <= 99999) //countOfDigit==5
+            //{
+            //    //int result = (400000 + a) * 10 + 2;
+            //    string s = $"4{a}2";
+            //    int result = int.Parse(s);
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("5 reqemli eded dacil etmelisiz");
+            //}
+            #endregion
+
+            #region Task 2
+
+            //int ay = int.Parse(Console.ReadLine());
+
+            //int il = ay / 12;
+            //int qalanAy = ay % 12;// ay - il*12;
+
+            //Console.WriteLine($"{il} il, {qalanAy} ay");
+
+
+            #endregion
+
+            decimal number = decimal.Parse(Console.ReadLine());
+            int tam = (int)number;
+            decimal kesr = number - tam;
+            kesr *= 1000000;
+            kesr += tam;
+            kesr /= 1000;
+            Console.WriteLine(kesr);
+        }
+    }
+}
